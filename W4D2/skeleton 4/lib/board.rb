57b7951be1b70f9d71f27785aedfc81651e09581
MichaelNgCen <michaelngcen@yahoo.com>
2022-06-13 23:33:07 -0400

    6 failure

--- W4D2/skeleton 4/lib/board.rb
+++ W4D2/skeleton 4/lib/board.rb
@@ -2,20 +2,52 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14) { Array.new }
+    place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |cup, i|
+      next if i == 6 || i == 13
+      4.times do
+        cup << :stone
+      end
+    end
   end
 
+
   def valid_move?(start_pos)
+    raise "Invalid starting cup" if start_pos < 0 || start_pos > 12
+    raise "Starting cup is empty" if @cups[start_pos].empty?
   end
 
   def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
+    cup_idx = start_pos
+    until stones.empty?
+      cup_idx += 1
+      cup_idx = 0 if cup_idx > 13
+      if cup_idx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      else
+        @cups[cup_idx] << stones.pop
+      end
+    end
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      :switch
+    else
+      ending_cup_idx
+    end
   end
 
   def render
@@ -27,8 +59,14 @@ class Board
   end
 
   def one_side_empty?
+    @cups[0..5].all? { |cup| cup.empty? } || @cups[7..12].all? { |cup| cup.empty? }
   end
 
   def winner
+    if @cups[6].count == @cups[13].count
+      :draw
+    else
+      @cups[6].count > @cups[13].count ? @name1 : @name2
+    end
   end
 end
